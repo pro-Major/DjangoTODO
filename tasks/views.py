52e2b2c7da4c1b1a38aa2387c99fad37d6daba28
Major <navikbipin@gmail.com>
2021-02-14 13:07:36 +0530

    form completed

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,21 @@
 # import here 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .forms import *
+from .models import *
+
 # Create your views here.
 
 def index(request): 
-    return render(request,'tasks/list.html')
+    tasks = Task.objects.all()
+
+    form = TaskForm()
+    if request.method =='POST':
+            form = TaskForm(request.POST)   
+            if form.is_valid():
+                form.save()
+            return redirect
+
+    context = {'tasks':tasks,'form':form}
+
+    return render(request,'tasks/list.html', context)
